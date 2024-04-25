547470f1b4e974b4377a3d399864673dbc6f6c2d
Arvid <arvid.harder@gmail.com>
2024-04-25 07:35:44 -0400

    implemented meta-analysis

--- R/meta-analysis.R
+++ R/meta-analysis.R
@@ -16,25 +16,29 @@ align_to_ref <- function(dset) {
     dplyr::select(-dplyr::all_of(c("EA_is_ref", "tmp")))
 
 }
-meta_analyze_by_chrom <- function(dset, chrom) {
+
+meta_analyze_by_chrom <- function(dset, chrom, by) {
+  stats <- c("B", "SE", "EAF", "N", "CaseN", "ControlN","INFO")
+  cols <- c(by, stats)
 
   dset |>
-    dplyr::filter(!is.na(ID)) |>
     dplyr::filter(CHR == {{ chrom }}) |>
+    dplyr::filter(!is.na(ID)) |>
     dplyr::filter(is.finite(B) & is.finite(SE)) |>
     align_to_ref() |>
-    dplyr::select(dplyr::any_of(c("ID", "B", "SE", "EAF", "N", "CaseN", "ControlN","INFO"))) |>
+    dplyr::select(dplyr::any_of(cols)) |>
     dplyr::mutate(
       W = 1 / (SE^2),
       B = B*W,
       dplyr::across(dplyr::any_of(c("EAF", "INFO")), ~.x * N)
     ) |>
-    dplyr::group_by(ID) |>
+    dplyr::group_by(dplyr::across(dplyr::all_of(by))) |>
     dplyr::summarise(
       n_contributions = dplyr::n(),
       dplyr::across(dplyr::any_of(c("W", "B")), sum),
       dplyr::across(dplyr::any_of(c("EAF", "INFO", "CaseN", "ControlN", "N")), ~sum(.x, na.rm=T))
     ) |>
+    dplyr::ungroup() |>
     dplyr::mutate(
       B = B / W,
       SE = 1 / sqrt(W),
@@ -45,6 +49,7 @@ meta_analyze_by_chrom <- function(dset, chrom) {
     dplyr::mutate(P  = stats::pnorm(-abs(B/SE)) *2)
 
 }
+
 
 
 
@@ -61,63 +66,13 @@ meta_analyze_by_chrom <- function(dset, chrom) {
 #' res <- meta_analyze(dset)
 #' }
 #'
-meta_analyze <- function(dset, method = c("ivw")) {
+meta_analyze <- function(dset, by = c("CHR", "POS", "RSID", "EffectAllele", "OtherAllele"), method = c("ivw")) {
   method <- rlang::arg_match(method)
-  purrr::map(c(1:22), \(chrom) meta_analyze_by_chrom(dset, chrom = chrom)) |>
+  purrr::map(c(1:22), \(chrom) meta_analyze_by_chrom(dset, chrom = chrom, by = by)) |>
     purrr::list_rbind()
 }
 
-#' Get the unique set of SNP, CHR, POS EffectAllele, OtherAllele, ID from a set of summary statistics
-#'
-#' @param dset a [arrow::open_dataset()] object
-#'
-#' @return a [dplyr::tibble()]
-#' @export
-#'
-#' @examples \dontrun{
-#' dset <- arrow::open_dataset("path_to_dsets")
-#' snpids <- get_snp_ids(dset)
-#' }
-get_snp_ids <- function(dset) {
-  purrr::map(c(1:22), \(chrom) get_snp_ids_chrom(dset, chrom = chrom)) |>
-    purrr::list_rbind()
-}
-
-get_snp_ids_chrom <- function(dset, chrom) {
-
-    dset |>
-      dplyr::filter(CHR == chrom) |>
-      dplyr::filter(!multi_allelic) |>
-      align_to_ref() |>
-      dplyr::select(RSID, CHR, POS, EffectAllele, OtherAllele, ID) |>
-      dplyr::distinct() |>
-      dplyr::collect()
-
-}
-
-# get_chr_pos_ea_oa <- function(df) {
-#   stopifnot("ID" %in% colnames(df))
-#   t <- stringi::stri_split_fixed(df$ID, ":", simplify = TRUE)
-#
-#   df$CHR <- t[,1]
-#   df$POS <- t[,2]
-#   df$EffectAllele <- t[,3]
-#   df$OtherAllele <-  t[,4]
-#
-#   dplyr::select(df, CHR,POS, EffectAllele, OtherAllele, dplyr::everything())
-#
-# }
 
 
 
-check_correct_cols <- function(dset) {
-  colnames <- names(dset$schema)
-  req_cols <- c("ID", "B", "SE", "CHR")
-  stopifnot(
-  "Missing either ID, B,SE or CHR" =
-    all(req_cols %in% colnames))
-
-
-
-}
 

0b6a050c991f3a26e8e5e7328967ceb89c9ed599
Arvid Harder <arvid.harder@gmail.com>
2024-09-10 14:51:22 +0200

    gene range list is now on GRCh38

--- R/plink.R
+++ R/plink.R
@@ -21,7 +21,7 @@ run_clumping <- function(path) {
 
     sumstat <- in_work_dir("sumstats.tsv")
     genome_ref <- in_ref_dir(paths$system_paths$genome_refs$deep_1kg)
-    gene_list <- in_ref_dir("plink/glist-hg19")
+    gene_list <- in_ref_dir("plink/glist-hg38")
 
     code <- clump_plink(
         sumstat = sumstat,
